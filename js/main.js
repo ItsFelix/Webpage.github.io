3602d4c0eda6404ee25cf8446a3d26397ef28c15
ItsFelix <f.samplexyt@gmail.com>
2018-10-21 19:54:28 +0200

    Moving Background

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+$('header').mousemove(function(e){
+      var moveX = (e.pageX * -1 / 50);
+      var moveY = (e.pageY * -1 / 50);
+      $(this).css('background-position', moveX + 'px ' + moveY + 'px');
+    });
+
 // ANimations animate.css waypoints.js
 $(document).ready(function(){
  
