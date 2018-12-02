e49a4f44d58d7f459f002defd7713e709acda323
ItsFelix <f.samplexyt@gmail.com>
2018-12-02 13:45:12 +0100

    remove mousemove background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$('header').mousemove(function(e){
+/*$('header').mousemove(function(e){
       var moveX = (e.pageX * -1 / 50);
       var moveY = (e.pageY * -1 / 50);
       $(this).css('background-position', moveX + 'px ' + moveY + 'px');
-    });
+    });*/
 
 // ANimations animate.css waypoints.js
 $(document).ready(function(){
